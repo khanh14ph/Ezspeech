8c26458099d01a1c6657df7dd0cdffb40715c6dc
khanh14ph <khanh14ph@gmail.com>
2025-01-25 15:34:07 +0700

    hehe

--- ezspeech/models/conformer_asr.py
+++ ezspeech/models/conformer_asr.py
@@ -145,32 +145,32 @@ class Conformer_self_condition_phoneme(torch.nn.Module):
     ) -> Tuple[torch.Tensor, torch.Tensor]:
 
         encoder_padding_mask = _lengths_to_padding_mask(lengths)
-        x = x.transpose(0, 1)
+        hidden = x.transpose(0, 1)
         inter_layer_softmax_lst=[]
         phoneme_inter_layer_softmax_lst=[]
         for idx,layer in enumerate(self.conformer_layers):
+            hidden=layer(hidden,encoder_padding_mask)
+            hidden=hidden.transpose(0, 1)
+            new_hidden=hidden
+            if idx in self.phoneme_inter_layer:
+                inter_layer_out=self.phoneme_lm_head(hidden)
+                phoneme_inter_prediction= inter_layer_out.log_softmax(2)
+                phoneme_inter_layer_in=self.phoneme_back_to_hidden(phoneme_inter_prediction)
+                phoneme_inter_layer_softmax_lst.append(phoneme_inter_prediction)
+                new_hidden=new_hidden+phoneme_inter_layer_in
             if idx in self.inter_layer:
-                x=layer(x,encoder_padding_mask)
-                x=x.transpose(0, 1)
-                inter_layer_out=self.lm_head(x)
+                
+                inter_layer_out=self.lm_head(hidden)
                 inter_prediction= inter_layer_out.log_softmax(2)
                 inter_layer_in=self.back_to_hidden(inter_prediction)
                 inter_layer_softmax_lst.append(inter_prediction)
-                x=x+inter_layer_in
-                x=x.transpose(1,0)
-            elif idx in self.phoneme_inter_layer:
-                x=layer(x,encoder_padding_mask)
-                x=x.transpose(0, 1)
-                inter_layer_out=self.phoneme_lm_head(x)
-                inter_prediction= inter_layer_out.log_softmax(2)
-                inter_layer_in=self.phoneme_back_to_hidden(inter_prediction)
-                phoneme_inter_layer_softmax_lst.append(inter_prediction)
-                x=x+inter_layer_in
-                x=x.transpose(1,0)
-            else:
-                x = layer(x, encoder_padding_mask)
-        last_hidden=x.transpose(0, 1)
-        x = self.lm_head(last_hidden)
-        x = x.log_softmax(2)
+                new_hidden=hidden+inter_layer_in
+                    
+            
 
-        return x, lengths,inter_layer_softmax_lst,phoneme_inter_layer_softmax_lst
+            hidden=new_hidden.transpose(1,0)
+        last_hidden=hidden.transpose(0, 1)
+        pred = self.lm_head(last_hidden)
+        pred = pred.log_softmax(2)
+
+        return pred, lengths,inter_layer_softmax_lst,phoneme_inter_layer_softmax_lst
