0d69cec8ab1e96dd2df697719bdf91e5d99c9739
khanh14ph <72318494+khanh14ph@users.noreply.github.com>
2025-02-10 08:29:01 +0700

    Update conformer_asr.py

--- ezspeech/models/conformer_asr.py
+++ ezspeech/models/conformer_asr.py
@@ -104,95 +104,6 @@ class Conformer_self_condition(torch.nn.Module):
 
         return x, lengths,inter_layer_softmax_lst
 
-class Conformer_self_condition_phoneme(torch.nn.Module):
-
-    def __init__(
-        self,
-        d_hidden: int,
-        num_heads: int,
-        num_layers: int,
-        depthwise_conv_kernel_size: int,
-        dropout: float = 0.0,
-        inter_layer: List[int]=[3,6,9],
-        phoneme_inter_layer: List[int]=[2,5,7],
-        vocab_size: int = 98,
-        phoneme_vocab_size: int= 1,
-
-    ):
-        super().__init__()
-        
-        self.conformer_layers = torch.nn.ModuleList(
-            [
-                ConformerLayer(
-                    d_hidden,
-                    4 * d_hidden,
-                    num_heads,
-                    depthwise_conv_kernel_size,
-                    dropout=dropout,
-                )
-                for _ in range(num_layers)
-            ]
-        )
-        self.inter_layer=inter_layer
-        # self.lm_head = torch.nn.Linear(d_hidden, vocab_size)
-        # self.back_to_hidden=torch.nn.Linear(vocab_size, d_hidden)
-        self.ln = nn.LayerNorm(d_hidden)
-        self.lm_head = torch.nn.Linear(d_hidden, vocab_size)
-
-        self.char_ln = nn.ModuleList([nn.LayerNorm(d_hidden) for _ in range(len(inter_layer))])
-        
-        self.char_lm_head = nn.ModuleList(nn.Linear(d_hidden,vocab_size) for i in range(len(inter_layer)))
-        
-
-        self.char_back_to_hidden = nn.ModuleList(nn.Linear(vocab_size,d_hidden) for i in range(len(inter_layer)))
-        
-        self.phoneme_inter_layer=phoneme_inter_layer
-        self.phoneme_ln = nn.ModuleList([nn.LayerNorm(d_hidden) for _ in range(len(phoneme_inter_layer))])  
-        self.phoneme_lm_head = nn.ModuleList(nn.Linear(d_hidden,phoneme_vocab_size) for i in range(len(phoneme_inter_layer)))   
-        self.phoneme_back_to_hidden=nn.ModuleList(nn.Linear(phoneme_vocab_size,d_hidden) for i in range(len(phoneme_inter_layer)))
-
-    def forward(
-        self, x: torch.Tensor, lengths: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-
-        encoder_padding_mask = _lengths_to_padding_mask(lengths)
-        hidden = x.transpose(0, 1)
-        inter_layer_softmax_lst=[]
-        phoneme_inter_layer_softmax_lst=[]
-        phoneme_inter_layer_count=0
-        inter_layer_count=0
-
-        for idx,layer in enumerate(self.conformer_layers):
-            hidden=layer(hidden,encoder_padding_mask)
-            hidden=hidden.transpose(0, 1)
-            new_hidden=hidden
-            if idx+1 in self.phoneme_inter_layer:
-                hidden=self.phoneme_ln[phoneme_inter_layer_count](hidden)
-                inter_layer_out=self.phoneme_lm_head[phoneme_inter_layer_count](hidden)
-                phoneme_inter_prediction= inter_layer_out.log_softmax(2)
-                phoneme_inter_layer_in=self.phoneme_back_to_hidden[phoneme_inter_layer_count](phoneme_inter_prediction)
-                phoneme_inter_layer_softmax_lst.append(phoneme_inter_prediction)
-                new_hidden=new_hidden+phoneme_inter_layer_in
-                phoneme_inter_layer_count+=1
-            if idx+1 in self.inter_layer:
-                hidden=self.char_ln[inter_layer_count](hidden)
-                inter_layer_out=self.char_lm_head[inter_layer_count](hidden)
-                inter_prediction= inter_layer_out.log_softmax(2)
-                inter_layer_in=self.char_back_to_hidden[inter_layer_count](inter_prediction)
-                inter_layer_softmax_lst.append(inter_prediction)
-                new_hidden=hidden+inter_layer_in
-                inter_layer_count+=1
-                    
-            
-
-            hidden=new_hidden.transpose(1,0)
-        last_hidden=hidden.transpose(0, 1)
-        last_hidden=self.ln(last_hidden)
-        pred = self.lm_head(last_hidden)
-        pred = pred.log_softmax(2)
-
-        return pred, lengths,inter_layer_softmax_lst,phoneme_inter_layer_softmax_lst
-
 class Conformer_self_condition_phoneme_share(torch.nn.Module):
 
     def __init__(
@@ -224,12 +135,10 @@ class Conformer_self_condition_phoneme_share(torch.nn.Module):
         self.inter_layer=inter_layer
         self.lm_head = torch.nn.Linear(d_hidden, vocab_size)
         self.back_to_hidden=torch.nn.Linear(vocab_size, d_hidden)
-
         self.ln =nn.LayerNorm(d_hidden) 
 
-
-        self.phoneme_ln=nn.LayerNorm(d_hidden) 
         self.phoneme_inter_layer=phoneme_inter_layer
+        self.phoneme_ln =nn.LayerNorm(d_hidden) 
         self.phoneme_lm_head = torch.nn.Linear(d_hidden, phoneme_vocab_size)
         self.phoneme_back_to_hidden=torch.nn.Linear(phoneme_vocab_size, d_hidden)
 
